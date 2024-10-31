a1888986b0c24bb9f8ad729398d06b774ad7b4ae
Yvss059 <rifiyvss@proton.me>
2024-10-31 16:42:08 +0100

    new

---
+++ App/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+# Importation de Flask et des Blueprints depuis les modules des routes
+from flask import Flask
+from routes.main_routes import main_bp
+
+# Création d'une application Flask
+app = Flask(__name__)
+
+# Enregistrement le blueprint dans l'application Flask
+app.register_blueprint(main_bp)
+
+# Exécution de l'application si elle est directement exécutée
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
